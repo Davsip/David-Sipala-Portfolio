2a41d384d271f86e47ee4c0205a2af739eabd8c6
DavSIp <david.sipala@gmail.com>
2019-02-21 17:18:27 -0600

    typed message

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
   $("#slides").superslides({
     animation: "fade",
-    play: 2000
+    play: 2500
     // pagination: false
+  });
+
+  var typed = new Typed(".typed", {
+    strings: [
+      "UX Designer.",
+      "User Research.",
+      "Product Designer",
+      "Web Developer."
+    ],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
   });
 });
