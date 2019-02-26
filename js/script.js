7bb211e749984e4deca6510f2f1dfbc9f9d94272
david sipala <david.sipala@gmail.com>
2019-02-26 09:12:01 -0600

    adding skills sections

--- js/script.js
+++ js/script.js
@@ -17,6 +17,68 @@ $(document).ready(function() {
     startDelay: 1000,
     showCursor: false
   });
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    items: 4,
+    responsive:{
+        0:{
+            items:1
+        },
+        480:{
+            items:2
+        },
+        768:{
+            items:3
+        },
+        938:{
+            items:4
+        }
+    }
+});
+
+
+    $('.chart').easyPieChart({
+        easing: 'easInOUt', 
+        barColor:'#3498db',
+        trackColor:false, 
+        scaleColor:false,
+        lineWidth:4,
+        size:152,
+        onStep: function(from, to, percent) {
+            $(this.el).find('.percent').text(Math.round(percent));
+        }
+
+    });
+
+
+    var skillsTopOffset = $(".skills-section").offset().top;
+
+	$(window).scroll(function() {
+
+		if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+
+			$('.chart').easyPieChart({
+		        easing: 'easeInOut',
+		        barColor: '#fff',
+		        trackColor: false,
+		        scaleColor: false,
+		        lineWidth: 4,
+		        size: 152,
+		        onStep: function(from, to, percent) {
+		        	$(this.el).find('.percent').text(Math.round(percent));
+		        }
+		    });
+			
+
+		}
+
+
+	});
+
+
+    
+
 });
 
 
